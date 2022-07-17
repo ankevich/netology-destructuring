fa2a3a4164f6bbe0d8c4bd06f668bd155b8ae0c3
Annie Bauer <overheads165@gmail.com>
2022-07-17 23:46:22 +0300

    destr

---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+const character = {
+  name: "Лучник",
+  type: "Bowman",
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: "Двойной выстрел",
+      icon: "http://...",
+      description: "Двойной выстрел наносит двойной урон",
+    },
+    {
+      id: 9,
+      name: "Нокаутирующий удар",
+      icon: "http://...",
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
+};
+
+const extract = ({ id, name, description = "Описание недоступно", icon }) => [
+  id,
+  name,
+  description,
+  icon,
+];
+
+const result = character.special.map((attack) => extract(attack));
+console.log(result)
